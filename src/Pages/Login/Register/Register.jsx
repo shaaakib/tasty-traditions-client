7a269f3cbf134a5cb598881a4a1fc93cadc66450
Muhammad Shakib Hossain <shakibhossainmuhammad@gmail.com>
2023-05-04 23:52:48 +0600

    added some design

--- src/Pages/Login/Register/Register.jsx
+++ src/Pages/Login/Register/Register.jsx
@@ -28,15 +28,6 @@ export default function Register() {
     setShowConfirmPass(!showConfirmPass);
   };
 
-  // const photoURL =
-  //   user.photoURL || 'https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg';
-
-  // if (user.photoURL) {
-  //   // use the user's photo URL
-  // } else {
-  //   // use a default photo or display a placeholder
-  // }
-
   const handleRegister = (event) => {
     event.preventDefault();
 
@@ -91,6 +82,8 @@ export default function Register() {
   const handleGoogleSignIn = () => {
     signInWithGoogle()
       .then((result) => {
+        logOut();
+        navigate('/login');
         const loggedUser = result.user;
         console.log(loggedUser);
       })
@@ -100,6 +93,8 @@ export default function Register() {
   const handleGitHubSignIn = () => {
     signInWithGitHub()
       .then((result) => {
+        logOut();
+        navigate('/login');
         const loggedUser = result.user;
         console.log(loggedUser);
       })
@@ -112,7 +107,7 @@ export default function Register() {
 
   return (
     <div>
-      <div className="bg-grey-lighter min-h-screen flex flex-col">
+      <div className="bg-grey-lighter min-h-screen flex flex-col ">
         <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
           <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
             <h1 className="mb-8 text-3xl text-center">Sign up</h1>
