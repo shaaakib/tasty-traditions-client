33c09d4fa6f9d59dfc37059bc588b16a6335e895
Muhammad Shakib Hossain <shakibhossainmuhammad@gmail.com>
2023-05-05 19:33:11 +0600

    width size fixed

--- src/Pages/Login/Login/Login.jsx
+++ src/Pages/Login/Login/Login.jsx
@@ -64,7 +64,7 @@ export default function Login() {
     <div>
       <div className="bg-grey-lighter min-h-screen flex flex-col">
         <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-          <div className="bg-white px-6 py-8 rounded shadow-md text-black w-[30rem]">
+          <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
             <h1 className="mb-8 text-3xl text-center">
               Sign in to your account
             </h1>
